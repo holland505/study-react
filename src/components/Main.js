123e4863a78297ee7ec666a2e87a71ff71c0f3a2
thurston <thurston303@gmail.com>
2023-01-29 16:57:30 +0900

    リファクタリング

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import { HeadLine } from '@/components/HeadLine'
+import styles from '../styles/Home.module.css'
+import { HeadLine } from '../components/HeadLine'
 
 const inter = Inter({ subsets: ['latin'] })
 
